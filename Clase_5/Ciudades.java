ae05195d691513f9f2db85b807fdff2d81c5e3fb
Eduardo <edulaffont@hotmail.com>
2023-10-05 01:09:38 -0500

    agregando la clase ciudades

---
+++ Clase_5/Ciudades.java
@@ -0,0 +1,36 @@
+package Clase_5;
+import java.util.Scanner;
+public class Ciudades {
+
+        public static void main(String[] args) {
+            Scanner ciudadscan = new Scanner(System.in);
+
+            // Arreglo de ciudades y sus apodos
+            String[] ciudades = {"Caracas", "Maracaibo", "Valencia", "Barquisimeto", "Merida"};
+            String[] apodos = {"la Ciudad de la Furia", "la Tierra del Sol Amada", "la Ciudad de los maricos", "la Ciudad Crepuscular", "la Ciudad de los Caballeros"};
+
+            // Mostrar las opciones disponibles al usuario
+            System.out.println("Selecciona una ciudad de Venezuela con un numero del 1 al 5 para obtener su apodo:");
+            for (int contador = 0; contador < ciudades.length; contador++) {
+                System.out.println((contador + 1) + ". " + ciudades[contador]);
+            }
+
+            // Leer la selección del usuario
+            int seleccion = ciudadscan.nextInt();
+
+            // Verificar que la selección esté dentro del rango válido
+            if (seleccion >= 1 && seleccion <= ciudades.length) {
+                // Mostrar el apodo de la ciudad seleccionada
+                String ciudadSeleccionada = ciudades[seleccion - 1];
+                String apodo = apodos[seleccion - 1];
+                System.out.println("El apodo de " + ciudadSeleccionada + " es " + apodo);
+            } else {
+                System.out.println("Selección no válida. Por favor, elige un número entre 1 y " + ciudades.length);
+            }
+
+            // Cerrar el objeto Scanner
+            ciudadscan.close();
+        }
+    }
+
+
--- Clase_5/Main.java
+++ Clase_5/Main.java
@@ -1,7 +1,21 @@
 package Clase_5;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-}
-}
+                Scanner scanner = new Scanner(System.in);
+
+                // Pedir al usuario que ingrese un país
+                System.out.print("Ingresa un país: ");
+                String pais = scanner.nextLine();
+
+                // Concatenar la URL de búsqueda en Google Maps
+                String url = "https://www.google.com/maps/search/" + pais;
+
+                // Mostrar la URL en la consola
+                System.out.println("URL de búsqueda en Google Maps: " + url);
+
+                // Cerrar el objeto Scanner
+                scanner.close();
+            }
+        }
